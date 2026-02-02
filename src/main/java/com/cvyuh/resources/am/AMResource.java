84493b29eb43878b3d390d31f86656ea9d5a981c
sitaram <sm@cvyuh.com>
2026-02-02 17:54:22 +0530

    true proxy

--- src/main/java/com/cvyuh/resources/am/AMResource.java
+++ src/main/java/com/cvyuh/resources/am/AMResource.java
@@ -1,19 +1,18 @@
 package com.cvyuh.resources.am;
 
 import com.cvyuh.resources.Constants;
-import com.cvyuh.service.am.AMHeader;
-import com.cvyuh.service.am.AMQuery;
 import com.cvyuh.service.am.AMService;
+import com.cvyuh.utils.log.LoggingContext;
 import com.cvyuh.utils.response.HttpMethod;
 import com.cvyuh.utils.response.ResponseHandler;
-import com.cvyuh.utils.log.LoggingContext;
-import io.vertx.core.json.JsonObject;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.*;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logging.Logger;
 import org.jboss.logging.MDC;
+
+import java.util.function.Function;
 
 @Path(Constants.CONTEXT.AM)
 public class AMResource implements ResponseHandler {
@@ -24,83 +23,62 @@ public class AMResource implements ResponseHandler {
     @RestClient
     AMService amService;
 
-    private String preProcess(UriInfo uriInfo, AMHeader amHeader, HttpHeaders httpHeaders) {
+    private String preProcess(UriInfo uriInfo) {
         MDC.put(LoggingContext.SERVICE, AMService.class.getName());
-
-        // DO NOT touch cookies
-        // No Authorization header derived from cookies here
-        String cookieHeader = httpHeaders.getHeaderString("Cookie");
-        if (cookieHeader != null && !cookieHeader.isEmpty()) {
-            amHeader.setCookie(cookieHeader);
-        }
-
         String subPath = uriInfo.getPath().substring(Constants.CONTEXT.AM.length());
         return "json" + subPath;
     }
 
     @GET
     @Path("{path:.*}")
-    public Response doGet(
-            @Context UriInfo uriInfo,
-            @BeanParam AMHeader header,
-            @BeanParam AMQuery query,
-            @Context HttpHeaders httpHeaders
-    ) {
-        String path = preProcess(uriInfo, header, httpHeaders);
-        return handleResponse(v -> amService.doGet(path, header, query), path, HttpMethod.GET, uriInfo.getQueryParameters());
+    public Response doGet(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders) {
+        String path = preProcess(uriInfo);
+        return handleResponse(
+                v -> amService.doGet(path, httpHeaders.getRequestHeaders(), uriInfo.getQueryParameters()),
+                path, HttpMethod.GET, uriInfo.getQueryParameters()
+        );
     }
 
     @POST
     @Path("{path:.*}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response doPost(
-            @Context UriInfo uriInfo,
-            @BeanParam AMHeader header,
-            @BeanParam AMQuery query,
-            @Context HttpHeaders httpHeaders,
-            String jsonBody
-    ) {
-        String path = preProcess(uriInfo, header, httpHeaders);
-        JsonObject jsonData = jsonBody != null && !jsonBody.trim().isEmpty() ? new JsonObject(jsonBody) : new JsonObject();
-        return handleResponse(v -> amService.doPost(path, header, query, jsonData.encode()), path, HttpMethod.POST, uriInfo.getQueryParameters());
+    public Response doPost(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String jsonBody) {
+        String path = preProcess(uriInfo);
+        return handleResponse(
+                v -> amService.doPost(path, httpHeaders.getRequestHeaders(), uriInfo.getQueryParameters(), jsonBody),
+                path, HttpMethod.POST, uriInfo.getQueryParameters()
+        );
     }
 
     @PUT
     @Path("{path:.*}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response doPut(
-            @Context UriInfo uriInfo,
-            @BeanParam AMHeader header,
-            @Context HttpHeaders httpHeaders,
-            String jsonBody
-    ) {
-        String path = preProcess(uriInfo, header, httpHeaders);
-        JsonObject jsonData = jsonBody != null && !jsonBody.trim().isEmpty() ? new JsonObject(jsonBody) : new JsonObject();
-        return handleResponse(v -> amService.doPut(path, header, jsonData.encode()), path, HttpMethod.PUT, uriInfo.getQueryParameters());
+    public Response doPut(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String jsonBody) {
+        String path = preProcess(uriInfo);
+        return handleResponse(
+                v -> amService.doPut(path, httpHeaders.getRequestHeaders(), jsonBody),
+                path, HttpMethod.PUT, null
+        );
     }
 
     @PATCH
     @Path("{path:.*}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response doPatch(
-            @Context UriInfo uriInfo,
-            @BeanParam AMHeader header,
-            @Context HttpHeaders httpHeaders,
-            String jsonBody
-    ) {
-        String path = preProcess(uriInfo, header, httpHeaders);
-        JsonObject jsonData = jsonBody != null && !jsonBody.trim().isEmpty() ? new JsonObject(jsonBody) : new JsonObject();
-        return handleResponse(v -> amService.doPatch(path, header, jsonData.encode()), path, HttpMethod.PATCH, uriInfo.getQueryParameters());
+    public Response doPatch(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders, String jsonBody) {
+        String path = preProcess(uriInfo);
+        return handleResponse(
+                v -> amService.doPatch(path, httpHeaders.getRequestHeaders(), jsonBody),
+                path, HttpMethod.PATCH, null
+        );
     }
 
     @DELETE
     @Path("{path:.*}")
-    public Response doDelete(
-            @Context UriInfo uriInfo,
-            @BeanParam AMHeader amHeader,
-            @Context HttpHeaders httpHeaders
-    ) {
-        String path = preProcess(uriInfo, amHeader, httpHeaders);
-        return handleResponse(v -> amService.doDelete(path, amHeader), path, HttpMethod.DELETE, uriInfo.getQueryParameters());
+    public Response doDelete(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders) {
+        String path = preProcess(uriInfo);
+        return handleResponse(
+                v -> amService.doDelete(path, httpHeaders.getRequestHeaders()),
+                path, HttpMethod.DELETE, null
+        );
     }
 }
--- src/main/java/com/cvyuh/service/am/AMService.java
+++ src/main/java/com/cvyuh/service/am/AMService.java
@@ -3,9 +3,12 @@ package com.cvyuh.service.am;
 import com.cvyuh.utils.exception.ExceptionWrapper;
 import com.cvyuh.utils.log.LoggingFilter;
 import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.jboss.resteasy.reactive.RestHeader;
+import org.jboss.resteasy.reactive.RestQuery;
 
 @Path("/")
 @RegisterProvider(LoggingFilter.class)
@@ -16,16 +19,16 @@ public interface AMService {
     @Path("{path: .+}")
     Response doGet(
             @PathParam("path") @Encoded String path,
-            @BeanParam AMHeader header,
-            @BeanParam AMQuery query
+            @RestHeader MultivaluedMap<String, String> headers,
+            @RestQuery MultivaluedMap<String, String> queryParams
     );
 
     @POST
     @Path("{path: .+}")
     Response doPost(
             @PathParam("path") @Encoded String path,
-            @BeanParam AMHeader header,
-            @BeanParam AMQuery query,
+            @RestHeader MultivaluedMap<String, String> headers,
+            @RestQuery MultivaluedMap<String, String> queryParams,
             String jsonBody
     );
 
@@ -33,7 +36,7 @@ public interface AMService {
     @Path("{path: .+}")
     Response doPut(
             @PathParam("path") @Encoded String path,
-            @BeanParam AMHeader header,
+            @RestHeader MultivaluedMap<String, String> headers,
             String jsonBody
     );
 
@@ -41,7 +44,7 @@ public interface AMService {
     @Path("{path: .+}")
     Response doPatch(
             @PathParam("path") @Encoded String path,
-            @BeanParam AMHeader header,
+            @RestHeader MultivaluedMap<String, String> headers,
             String jsonBody
     );
 
@@ -49,6 +52,6 @@ public interface AMService {
     @Path("{path: .+}")
     Response doDelete(
             @PathParam("path") @Encoded String path,
-            @BeanParam AMHeader header
+            @RestHeader MultivaluedMap<String, String> headers
     );
 }
